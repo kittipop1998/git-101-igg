d9479c91340adc811eadf6a23c76a131a286f96a
Wine Kittipop <37920511+kittipop1998@users.noreply.github.com>
2020-09-16 15:43:50 +0700

    syncAndAsync.js

--- js/quiz3.js
+++ js/quiz3.js
@@ -1,8 +1,18 @@
 const calculator = (point) => {
     let grade = null
-    if (point >= 81 point >=100) {
+    if (point >= 81 point && >=100) {
         grade = "A"
+    } else if (point >=71 && point >80){
+        grade = "B"
+    } else if (point >=61 && point >70){
+        grade = "C"
+    } else if (point >=51 && point >60){
+        grade = "D"
+    } else if (point >=0 && point >50){
+        grade = "F"
+    } else{
+        grade = "Point was invalid"
     }
-    else if (point >=71 )
-    }
+    return grade
 }
+console.log(calculateGrad(50));
---
+++ js/quiz4.js
@@ -0,0 +1,9 @@
+//1
+var txt = "Hello my name is Joke";
+var sln = txt.length;
+console.log(sln);
+
+//2
+var txt = "Hello my name is Joke";
+var sln = txt.length;
+console.log(sln);
