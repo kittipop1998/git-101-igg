2a661b7adeb94b185305df23c65b9ea3420cf675
Wine Kittipop <37920511+kittipop1998@users.noreply.github.com>
2020-09-16 19:18:14 +0700

    Update quiz3.js

--- js/quiz3.js
+++ js/quiz3.js
@@ -1,18 +1,18 @@
-const calculator = (point) => {
+const calculateGrade = (point) => {
     let grade = null
-    if (point >= 81 point && >=100) {
+    if (point >= 80 && point  <=100) {
         grade = "A"
-    } else if (point >=71 && point >80){
+    } else if(point >=70 && point <= 79){
         grade = "B"
-    } else if (point >=61 && point >70){
+    } else if(point >=60 && point <= 69){
         grade = "C"
-    } else if (point >=51 && point >60){
+    } else if(point >=50 && point <= 59){
         grade = "D"
-    } else if (point >=0 && point >50){
+    } else if(point >=0 && point <= 49){
         grade = "F"
-    } else{
-        grade = "Point was invalid"
+    } else {
+        grade = "คะแนนไม่ถูกต้อง"
     }
     return grade
 }
-console.log(calculateGrad(50));
+console.log(calculateGrade(80))
