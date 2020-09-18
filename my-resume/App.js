55c471d445e17de35bf8045208a2b742e6ff4bcd
Wine Kittipop <37920511+kittipop1998@users.noreply.github.com>
2020-09-18 16:09:52 +0700

    commit

--- my-resume/App.js
+++ my-resume/App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ResumeFormScreen from './screens/ResumeForm'
 import ResumeDetailScreen from './screens/ResumeDetail'
+import ResumeListScreen from './screens/ResumeList'
 
 function HomeScreen() {
   return (
@@ -21,6 +22,7 @@ function App() {
       <Stack.Navigator>
         <Stack.Screen name="ResumeForm" options={{ title: 'Resume Form'}} component={ResumeFormScreen} />
         <Stack.Screen name="ResumeDetail" options={{ title: 'Resume Detail'}} component={ResumeDetailScreen} />
+        <Stack.Screen name="ResumeList" options={{ title: 'Resume List'}} component={ResumeListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
